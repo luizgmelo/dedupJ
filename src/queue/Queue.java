50b5a9f399aef1cb5b6064ee1dfca1e2740afb30
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-03-22 12:04:53 -0300

    feat: add Node Generic

---
+++ src/queue/Queue.java
@@ -0,0 +1,26 @@
+package queue;
+
+
+
+
+
+class Node<T> {
+	private T data;
+	private Node<T> next;
+	
+	public Node(T data) {
+		this.data = data;
+	}
+	
+	public T getData() {
+		return this.data;
+	}
+	
+	public Node<T> getNext() {
+		return this.next;
+	}
+	
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+}
