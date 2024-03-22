c7311f45b6f95bad7d85db28f00e2fef91ed1bef
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-03-22 12:05:47 -0300

    feat: add Queue constructors

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -1,5 +1,23 @@
 package queue;
 
+public class Queue<T> {
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+	
+	
+	public Queue() {
+		
+	}
+	
+	public Queue(T element) {
+		Node<T> node = new Node<T>(element);
+		this.head = node;
+		this.tail = node;
+		this.size = 1;
+	}
+	
+}
 
 
 
